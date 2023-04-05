3ac80b22dd9fd171ab9c9c6b32089e1375191303
blasferna <blasferna@gmail.com>
2023-04-05 14:07:48 -0400

    feat: add xhtml2pdf converter

--- app/service.py
+++ app/service.py
@@ -1,8 +1,11 @@
+from io import BytesIO
 from typing import List
 
 import requests
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import Response
+from xhtml2pdf import pisa
 
 app = FastAPI()
 app.add_middleware(
@@ -23,3 +26,15 @@ async def get(url):
     """
     r = requests.get(url, timeout=(10, 10))
     return {"url": r.url}
+
+
+@app.post("/xhtml2pdf")
+async def convert_html_to_pdf(html: str = Form()):
+    pdf_buffer = BytesIO()
+    pisa.CreatePDF(html, dest=pdf_buffer)
+    pdf_buffer.seek(0)
+    filename = "example.pdf"
+    response = Response(pdf_buffer.read(), media_type='application/pdf')
+    response.headers["Content-Disposition"] = f"attachment; filename={filename}"
+    return response
+
