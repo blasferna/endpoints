5a70b3b542f157c5c04efbec28348658166d08a7
blasferna <blasferna@gmail.com>
2023-07-08 08:31:08 -0400

    feat: og-image endpoint

--- app/service.py
+++ app/service.py
@@ -160,8 +160,8 @@ async def tts(text, lang="en"):
 
 
 @app.get("/og-image")
-async def create_og_image(title: str):
-    fontSize =  '96px' if len(title) < 60 else '72px'
+async def create_og_image(title: str, sitename: str):
+    fontSize =  '86px' if len(title) < 70 else '62px'
     html = f"""
     <html>
         <head>
@@ -169,27 +169,34 @@ async def create_og_image(title: str):
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <style>
                 body {{
-                    background: #d3d9fd;
-                    color: #000100;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
+                    background: rgb(248,248,255);
+                    background: radial-gradient(circle, rgba(248,248,255,1) 22%, rgba(211,223,255,0.999019676229867) 100%); 
+                    color: #080707;
                     height: 100vh;
                     margin: 0;
                     font-family: Arial, sans-serif;
+                    padding: 50px;
                 }}
             </style>
         </head>
         <body>
-            <h1 style="max-width: 300px">{title}</h1>
+            <h1 style="text-align: left; font-size: {fontSize}; max-width: 80%;" >{title}</h1>
+            <div style="bottom: 50px; left: 50px; position: absolute; display: flex; align-items:center;">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" style="width: 30px; height: 30px; margin-right: 5px; color: #3574ea;">
+                    <path d="M21.721 12.752a9.711 9.711 0 00-.945-5.003 12.754 12.754 0 01-4.339 2.708 18.991 18.991 0 01-.214 4.772 17.165 17.165 0 005.498-2.477zM14.634 15.55a17.324 17.324 0 00.332-4.647c-.952.227-1.945.347-2.966.347-1.021 0-2.014-.12-2.966-.347a17.515 17.515 0 00.332 4.647 17.385 17.385 0 005.268 0zM9.772 17.119a18.963 18.963 0 004.456 0A17.182 17.182 0 0112 21.724a17.18 17.18 0 01-2.228-4.605zM7.777 15.23a18.87 18.87 0 01-.214-4.774 12.753 12.753 0 01-4.34-2.708 9.711 9.711 0 00-.944 5.004 17.165 17.165 0 005.498 2.477zM21.356 14.752a9.765 9.765 0 01-7.478 6.817 18.64 18.64 0 001.988-4.718 18.627 18.627 0 005.49-2.098zM2.644 14.752c1.682.971 3.53 1.688 5.49 2.099a18.64 18.64 0 001.988 4.718 9.765 9.765 0 01-7.478-6.816zM13.878 2.43a9.755 9.755 0 016.116 3.986 11.267 11.267 0 01-3.746 2.504 18.63 18.63 0 00-2.37-6.49zM12 2.276a17.152 17.152 0 012.805 7.121c-.897.23-1.837.353-2.805.353-.968 0-1.908-.122-2.805-.353A17.151 17.151 0 0112 2.276zM10.122 2.43a18.629 18.629 0 00-2.37 6.49 11.266 11.266 0 01-3.746-2.504 9.754 9.754 0 016.116-3.985z" />
+                </svg>
+                <span style="font-size: 30px; font-weight: 800;">
+                {sitename}
+                </span>
+            </div>
         </body>
     </html>
     """
     options = {
         'format': 'jpeg',
-        'width': 600,
-        'height': 315,
+        'width': 1200,
+        'height': 630,
         'quality': 100
     }
-    imgkit.from_string(html, 'out.jpg', options=options)
-    return FileResponse('out.jpg', media_type='image/jpeg')
+    image = imgkit.from_string(html, False, options=options)
+    return Response(image, media_type="image/jpeg")
