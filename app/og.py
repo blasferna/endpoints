ae2c3fc7501f8e6a7d864238c5a9495cf39b160b
blasferna <blasferna@gmail.com>
2023-07-09 14:43:52 -0400

    feat: generate open graph image in pure Python

--- app/og.py
+++ app/og.py
@@ -1,18 +1,18 @@
-import requests
 import os
-import tempfile
+from tempfile import TemporaryFile
 import zipfile
-from PIL import Image, ImageDraw, ImageFont
-import cairosvg
+from io import BytesIO
 
-# Define el directorio de caché para las fuentes
+import requests
+from PIL import Image, ImageDraw, ImageFont
+
 FONT_CACHE_DIR = "./font_cache/"
 
 
 def create_gradient(width, height, top_color, bottom_color):
-    base = Image.new('RGB', (width, height), top_color)
-    top = Image.new('RGB', (width, height), bottom_color)
-    mask = Image.new('L', (width, height))
+    base = Image.new("RGB", (width, height), top_color)
+    top = Image.new("RGB", (width, height), bottom_color)
+    mask = Image.new("L", (width, height))
     mask_data = []
     for y in range(height):
         mask_data.extend([int(255 * (y / height))] * width)
@@ -22,38 +22,55 @@ def create_gradient(width, height, top_color, bottom_color):
 
 
 def download_font(font_name: str):
-    os.makedirs(FONT_CACHE_DIR, exist_ok=True)
+    font_directory = FONT_CACHE_DIR
 
-    font_path = FONT_CACHE_DIR + font_name
-
-    if not os.path.isfile(font_path):
+    if not os.path.isdir(font_directory):
         url = f"https://fonts.google.com/download?family={font_name}"
-        response = requests.get(url)
-        
-        tmp = tempfile.NamedTemporaryFile(delete=False)
-        tmp.write(response.content)
+        response = requests.get(url, stream=True)
+
+        tmp = TemporaryFile()
+        for chunk in response.iter_content(chunk_size=32768):
+            tmp.write(chunk)
+
+        zip = zipfile.ZipFile(tmp)
+        files = [
+            file
+            for file in zip.namelist()
+            if file.endswith(".ttf") or file.endswith(".otf")
+        ]
+        zip.extractall(FONT_CACHE_DIR, files)
+
         tmp.close()
 
-        with zipfile.ZipFile(tmp.name, 'r') as zip_ref:
-            for file in zip_ref.namelist():
-                if file.endswith(".ttf") or file.endswith(".otf"):
-                    zip_ref.extract(file, FONT_CACHE_DIR)
-                    os.rename(FONT_CACHE_DIR + file, font_path)
-                    break
 
-        os.remove(tmp.name)
+def get_image(name, thumbnail: tuple = None, opacity=1):
+    with open(f"app/resources/img/{name}.png", "rb") as f:
+        image_bytes = f.read()
 
-    return font_path
+    image_stream = BytesIO(image_bytes)
+    image = Image.open(image_stream)
+    if thumbnail:
+        image.thumbnail(thumbnail)
+    rgba = image.convert("RGBA")
+
+    if opacity != 1 and opacity <= 1 and opacity >= 0:
+        for x in range(rgba.width):
+            for y in range(rgba.height):
+                r, g, b, a = rgba.getpixel((x, y))
+                rgba.putpixel((x, y), (r, g, b, int(a * opacity)))
+
+    return rgba
+
 
 def wrap_text(text, width, font):
     lines = []
     if font.getsize(text)[0] <= width:
         lines.append(text)
     else:
-        words = text.split(' ')
+        words = text.split(" ")
         i = 0
         while i < len(words):
-            line = ''
+            line = ""
             while i < len(words) and font.getsize(line + words[i])[0] <= width:
                 line = line + words[i] + " "
                 i += 1
@@ -63,48 +80,52 @@ def wrap_text(text, width, font):
             lines.append(line)
     return lines
 
-def generate_og_image(title: str, site_name: str, top_color: tuple, bottom_color: tuple, 
-                      font_name: str, svg_path: str, output_path: str):
-    # Descarga la fuente y obtén la ruta del archivo
-    font_path = download_font(font_name)
-    
-    # Define el tamaño de la imagen
-    img_width, img_height = 1200, 630  # Tamaño estándar para las imágenes Open Graph
 
-    # Create the gradient background
-    img = create_gradient(img_width, img_height, top_color, bottom_color)
-    
-    # Crea un objeto ImageDraw
+def generate_og_image(
+    title: str,
+    site_name: str,
+    top_color: tuple = (206, 236, 255),
+    bottom_color: tuple = (236, 248, 255),
+    font_name: str = "Roboto",
+    tag: str = None,
+):
+    download_font(font_name)
+
+    img_width, img_height = 1200, 630 
+
+    img = create_gradient(img_width, img_height, top_color, bottom_color).convert(
+        "RGBA"
+    )
     d = ImageDraw.Draw(img)
 
-    # Define las fuentes
-    title_font = ImageFont.truetype(font_path, 60)
-    site_font = ImageFont.truetype(font_path, 30)
+    title_font = ImageFont.truetype(FONT_CACHE_DIR + font_name + "-Medium.ttf", 70)
+    site_font = ImageFont.truetype(FONT_CACHE_DIR + font_name + "-Regular.ttf", 30)
 
-    # Dibuja el título y el nombre del sitio en la imagen
-    title_lines = wrap_text(title, 0.8 * img_width - 2*50, title_font)
-    title_y = 50  # Start at top padding for title
+    code_logo = get_image("code", (120, 120))
+    img.paste(code_logo, (45, 50), code_logo)
+
+    title_lines = wrap_text(title, 0.8 * img_width - 2 * 50, title_font)
+    title_y = 200 
+
+    line_spacing = 10
 
     for line in title_lines:
-        d.text((50, title_y), line, fill=(0, 0, 0), font=title_font)
-        title_y += title_font.getsize('A')[1]
-
-    # Position the icon and site name at the bottom left with 50px padding
-    cairosvg.svg2png(url=svg_path, write_to='temp.png')
-    icon = Image.open('temp.png')
-    icon.thumbnail((50, 50))
+        d.text((50, title_y), line, fill="#252525", font=title_font)
+        title_y += title_font.getsize("A")[1] + line_spacing
 
     site_x = 50
-    site_y = img_height - site_font.getsize('A')[1] - 50
+    site_y = img_height - site_font.getsize("A")[1] - 50
 
-    img.paste(icon, (site_x, site_y))
+    d.text((site_x, site_y), site_name, fill="#585858", font=site_font)
 
-    site_x += icon.width + 10
-    d.text((site_x, site_y), site_name, fill="#000000", font=site_font)
+    if tag:
+        try:
+            tag_image = get_image(tag, opacity=0.5)
+            img.paste(tag_image, (894, 324), tag_image)
+        except FileNotFoundError:
+            pass
 
-    # Guarda la imagen
-    img.save(output_path)
-
-# Llamada a la función con los parámetros requeridos
-generate_og_image('Este es uno de lo mejores ejemplo de que podemos lograr algo grando con muy poco', 'blasferna.com', 
-                  (146, 151, 255), (222, 224, 252), 'Roboto', 'world_icon.svg', 'og_image.png')
+    img_file = BytesIO()
+    img.save(img_file, "png")
+    img_file.seek(0)
+    return img_file
--- app/service.py
+++ app/service.py
@@ -3,16 +3,17 @@ from functools import wraps
 from io import BytesIO
 from typing import List, Union
 
-import imgkit
 import requests
 from fastapi import FastAPI, Form, HTTPException
-from fastapi.responses import FileResponse, StreamingResponse
+from fastapi.responses import StreamingResponse
 from gtts import gTTS, gTTSError
 from gtts.lang import tts_langs
 from pydantic import BaseModel, BaseSettings
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import Response
 from xhtml2pdf import pisa
+
+from app.og import generate_og_image
 
 
 class Settings(BaseSettings):
@@ -159,44 +160,11 @@ async def tts(text, lang="en"):
     return StreamingResponse(mp3, media_type="audio/mp3")
 
 
-@app.get("/og-image")
-async def create_og_image(title: str, sitename: str):
-    fontSize =  '86px' if len(title) < 70 else '62px'
-    html = f"""
-    <html>
-        <head>
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <style>
-                body {{
-                    background: rgb(248,248,255);
-                    background: radial-gradient(circle, rgba(248,248,255,1) 22%, rgba(211,223,255,0.999019676229867) 100%); 
-                    color: #080707;
-                    height: 100vh;
-                    margin: 0;
-                    font-family: Arial, sans-serif;
-                    padding: 50px;
-                }}
-            </style>
-        </head>
-        <body>
-            <h1 style="text-align: left; font-size: {fontSize}; max-width: 80%;" >{title}</h1>
-            <div style="bottom: 50px; left: 50px; position: absolute; display: flex; align-items:center;">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" style="width: 30px; height: 30px; margin-right: 5px; color: #3574ea;">
-                    <path d="M21.721 12.752a9.711 9.711 0 00-.945-5.003 12.754 12.754 0 01-4.339 2.708 18.991 18.991 0 01-.214 4.772 17.165 17.165 0 005.498-2.477zM14.634 15.55a17.324 17.324 0 00.332-4.647c-.952.227-1.945.347-2.966.347-1.021 0-2.014-.12-2.966-.347a17.515 17.515 0 00.332 4.647 17.385 17.385 0 005.268 0zM9.772 17.119a18.963 18.963 0 004.456 0A17.182 17.182 0 0112 21.724a17.18 17.18 0 01-2.228-4.605zM7.777 15.23a18.87 18.87 0 01-.214-4.774 12.753 12.753 0 01-4.34-2.708 9.711 9.711 0 00-.944 5.004 17.165 17.165 0 005.498 2.477zM21.356 14.752a9.765 9.765 0 01-7.478 6.817 18.64 18.64 0 001.988-4.718 18.627 18.627 0 005.49-2.098zM2.644 14.752c1.682.971 3.53 1.688 5.49 2.099a18.64 18.64 0 001.988 4.718 9.765 9.765 0 01-7.478-6.816zM13.878 2.43a9.755 9.755 0 016.116 3.986 11.267 11.267 0 01-3.746 2.504 18.63 18.63 0 00-2.37-6.49zM12 2.276a17.152 17.152 0 012.805 7.121c-.897.23-1.837.353-2.805.353-.968 0-1.908-.122-2.805-.353A17.151 17.151 0 0112 2.276zM10.122 2.43a18.629 18.629 0 00-2.37 6.49 11.266 11.266 0 01-3.746-2.504 9.754 9.754 0 016.116-3.985z" />
-                </svg>
-                <span style="font-size: 30px; font-weight: 800;">
-                {sitename}
-                </span>
-            </div>
-        </body>
-    </html>
-    """
-    options = {
-        'format': 'jpeg',
-        'width': 1200,
-        'height': 630,
-        'quality': 100
-    }
-    image = imgkit.from_string(html, False, options=options)
-    return Response(image, media_type="image/jpeg")
+@app.get(
+    "/og-image",
+    responses={200: {"content": {"image/png": {}}}},
+    response_class=Response,
+)
+async def create_og_image(title: str, sitename: str, tag: str):
+    image = generate_og_image(title, sitename, tag=tag)
+    return Response(image.read(), media_type="image/png")
